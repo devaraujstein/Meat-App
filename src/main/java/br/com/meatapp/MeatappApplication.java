e0127e39a9ec108e2251a4e616170063750ae5a9
AndreAraujoDev <andreearaujo98@gmail.com>
2019-08-07 20:57:58 -0300

    Iniciando o projeto Meat App

---
+++ src/main/java/br/com/meatapp/MeatappApplication.java
@@ -0,0 +1,13 @@
+package br.com.meatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatappApplication.class, args);
+	}
+
+}
