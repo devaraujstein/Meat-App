59b8beaf5495ed45839ccafded5a55ef442490f3
AndreAraujoDev <andreearaujo98@gmail.com>
2019-08-19 21:41:59 -0300

    Criando o Repositories e Service

---
+++ src/main/java/br/com/meatapp/service/UserService.java
@@ -0,0 +1,19 @@
+package br.com.meatapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.meatapp.domain.User;
+import br.com.meatapp.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository userRepository;
+	
+	public List<User> findAll(){
+		return userRepository.findAll();
+	}
+}
