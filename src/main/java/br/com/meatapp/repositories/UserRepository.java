12bc47f5b0fa3e7920f870bf689ba55c9d583633
AndreAraujoDev <andreearaujo98@gmail.com>
2019-08-28 21:46:24 -0300

    Encontrar Usuario por Email e Inserindo Usuario

--- src/main/java/br/com/meatapp/repositories/UserRepository.java
+++ src/main/java/br/com/meatapp/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package br.com.meatapp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import br.com.meatapp.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,Integer> {
 	
-	public User findByEmail(String email);
+	public Optional<User> findByEmail(String email);
 }
