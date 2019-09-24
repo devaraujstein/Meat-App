710314f4ee17d110282f60d6851b9fedda5c8c75
AndreAraujoDev <andreearaujo98@gmail.com>
2019-09-23 21:44:05 -0300

    Creando o DBService e configurações

--- src/main/java/br/com/meatapp/config/DevConfig.java
+++ src/main/java/br/com/meatapp/config/DevConfig.java
@@ -1,10 +1,26 @@
 package br.com.meatapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import br.com.meatapp.service.DBService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
-
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String ddl;
+	
+	public void instanciaDev() {
+		if(ddl.equals("create-drop")) {
+			dbService.instanciaDatabase();
+		}
+		
+	}
 }
--- src/main/java/br/com/meatapp/config/TestConfig.java
+++ src/main/java/br/com/meatapp/config/TestConfig.java
@@ -1,10 +1,21 @@
 package br.com.meatapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import br.com.meatapp.service.DBService;
 
 @Configuration
 @Profile("test")
 public class TestConfig {
 	
+	@Autowired
+	private DBService dbservice;
+	
+	@Bean
+	public void instanciaTeste() {
+		dbservice.instanciaDatabase();
+	}
 }
